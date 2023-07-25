3b8053afb17a446bc849c99c60df65ac496a70b7
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2023-07-26 00:25:10 +0300

    Remove requirement.txt read in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setup(
     name='apkinjector',
-    version='1.0.2',
+    version='1.0.3',
     description='Utilities to help injecting libraries and frida in apks.',
     long_description=long_description,
     long_description_content_type='text/markdown',
