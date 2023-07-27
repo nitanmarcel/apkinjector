3b82d6466dd4b41bcf11231aaee3a2b0b136620a
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2023-07-27 20:54:24 +0300

    Version 1.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='apkinjector',
-    version='1.0.4',
+    version='1.1.0',
     description='Utilities to help injecting libraries and frida in apks.',
     long_description=long_description,
     long_description_content_type='text/markdown',
